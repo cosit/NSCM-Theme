0cd9ab876de0aea8a6dad304fecf20d62d0c0da6
Jonathan <jhendricker@gmail.com>
2024-01-25 17:00:17 -0500

    Created custom layout for the UCF Post List plugin
    I created a custom view for the UCF Post List plugin that mimics the "modern" layout found in the UCF News plugin. This will make is simpler to display posts of different categories within the NSCM website. This new feature requires the activation of the UCF Post List plugin.

--- functions.php
+++ functions.php
@@ -264,4 +264,184 @@ add_filter('excerpt_more', function($more) {
 	return '...';
 });
 
+/**
+ * Add a new 'modern' layout for the UCF Post List plugin
+ *  
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ */
+if ( ! function_exists( 'nscm_add_modern_layout' ) ) {
+	function nscm_add_modern_layout( $layouts ) {
+		$layouts['modern'] = 'Modern Layout';
+		return $layouts;
+	}
+	add_action( 'ucf_post_list_get_layouts', 'nscm_add_modern_layout', 10, 1 );
+}
+
+/**
+ * Add a new 'modern' layout before function for the UCF Post List plugin
+ *  
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ */
+if ( !function_exists( 'ucf_post_list_display_modern_before' ) ) {
+
+	function ucf_post_list_display_modern_before( $content, $posts, $atts ) {
+		ob_start();
+	?>
+	<div class="ucf-post-list ucf-post-list-modern" id="post-list-<?php echo $atts['list_id']; ?>">
+	<?php
+		return ob_get_clean();
+	}
+
+	add_filter( 'ucf_post_list_display_modern_before', 'ucf_post_list_display_modern_before', 10, 3 );
+
+}
+
+/**
+ * Add a new 'modern' layout title function for the UCF Post List plugin
+ *  
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ */
+if ( !function_exists( 'ucf_post_list_display_modern_title' ) ) {
+
+	function ucf_post_list_display_modern_title( $content, $posts, $atts ) {
+		$formatted_title = '';
+
+		if ( $list_title = $atts['list_title'] ) {
+			$formatted_title = '<h2 class="ucf-post-list-title">' . $list_title . '</h2>';
+		}
+
+		return $formatted_title;
+	}
+
+	add_filter( 'ucf_post_list_display_modern_title', 'ucf_post_list_display_modern_title', 10, 3 );
+
+}
+
+/**
+ * Add a new 'modern' layout display function for the UCF Post List plugin
+ *  
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ */
+if ( !function_exists( 'ucf_post_list_display_modern' ) ) {
+
+	function ucf_post_list_display_modern( $content, $posts, $atts ) {
+		if ( ! is_array( $posts ) && $posts !== false ) { $posts = array( $posts ); }
+		ob_start();
+	
+		// Display only if there are found posts and the UCF Post List Common class has been instantiated
+		if ( $posts && class_exists( 'UCF_Post_List_Common' ) ): ?>
+			
+			<?php foreach ( $posts as $item ): 
+
+			$date = "<em>".date( "M d, Y", strtotime( $item->post_date ) )."</em>";
+			$size = 'thumbnail';
+
+			$categories = get_the_category($item);
+			$separator = ' ';
+			$category_output = '';
+			if ( ! empty( $categories ) ) {
+				foreach( $categories as $category ) {
+					$category_output .= '<span class="badge badge-primary mb-1">' . esc_html( $category->name ) . '</span>' . $separator;
+				}				
+			}
+
+			// Calling a newly created function to handle getting the image. This differs from the default because it returns a specific size for the fallback image and not the full size.
+			$item_img = nscm_get_image_or_fallback( $item, $size );
+			//$item_img_srcset = UCF_Post_List_Common::get_image_srcset( $item );
+						
+			$char_limit = $atts['excerpt_length'];
+			$item_excerpt= UCF_Post_List_Common::get_excerpt( $item, $char_limit );
+			?>
+
+			<div class="media-background-container hover-parent p-3 mb-3" style="margin-left: -1rem; margin-right: -1rem;">
+				<div class="media-background hover-child-show fade" style="background-color: rgba(204, 204, 204, .25);"></div>
+
+				<div class="media">
+					<?php if ( $item_img ) : ?>
+					<div class=" d-flex w-25 mr-3" style="max-width: 150px;">
+						<img src="<?php echo $item_img; ?>" class="ucf-post-list-thumbnail-image" alt="<?php echo $item->post_title; ?>">
+					</div>
+					<?php endif; ?>
+					<div class=" media-body">
+						<div>
+							<a class="d-block stretched-link text-decoration-none h5 mb-0 pb-1" href="<?php echo get_permalink( $item->ID ); ?>" style="color: inherit;">
+								<?php echo $item->post_title; ?>
+							</a>
+							<?php 
+								echo ( !empty($category_output) ? "<div class='font-size-sm'>".trim( $category_output, $separator )."</div>" : '' );
+
+								echo ( !empty($date) ? "<div class='font-size-sm mb-2'>$date</div>" : '' );								
+							?>								
+							<div>
+								<?php echo wp_trim_words( $item_excerpt, 25 ); ?>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<?php endforeach; ?>
+
+		<?php else: ?>
+			<div class="ucf-post-list-error">No results found.</div>
+		<?php endif; ?>
+	<?php
+		return ob_get_clean();
+	}
+	add_filter( 'ucf_post_list_display_modern', 'ucf_post_list_display_modern', 10, 3 );
+
+}
+
+/**
+ * Add a new 'modern' layout after function for the UCF Post List plugin
+ *  
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ */
+if ( !function_exists( 'ucf_post_list_display_modern_after' ) ) {
+
+	function ucf_post_list_display_modern_after( $content, $posts, $atts ) {
+		ob_start();
+	?>
+	</div>
+	<?php
+		return ob_get_clean();
+	}
+
+	add_filter( 'ucf_post_list_display_modern_after', 'ucf_post_list_display_modern_after', 10, 3 );
+
+}
+
+/**
+ * Alternative to UCF_Post_List_Common::get_image_or_fallback that will 
+ * return a specific size for the fallback image instead of the full 
+ * size fallback image.
+ * 
+ * @author	Jonathan Hendricker
+ * @since	1.0.21
+ * @param object $item | WP_Post object
+ * @param mixed $size | image size (accepts any valid image size, or an array of width and height values in pixels, in that order)
+ * @return string | image url
+ */
+if ( !function_exists( 'nscm_get_image_or_fallback' ) ) {
+	function nscm_get_image_or_fallback( $item, $size='large' ) {
+		$img    = null;
+		$img_id = UCF_Post_List_Common::get_image_id_or_fallback( $item );
+
+		if ( $img_id !== intval( UCF_Post_List_Config::get_option_or_default( 'ucf_post_list_fallback_image' ) ) ) {
+			$img = wp_get_attachment_image_src( $img_id, $size );
+			$img = $img ? $img[0] : null;
+		}
+
+		if ( $img === null ) {
+			$img = wp_get_attachment_image_url( $img_id, $size );
+		}
+
+		return $img;
+	}
+}
 ?>
